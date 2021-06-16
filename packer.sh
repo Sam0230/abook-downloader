c60e14d5fefb68e4557436449a65a91e030ffa61
Sam0230 <31580682+Sam0230@users.noreply.github.com>
2021-06-16 17:01:49 +0800

    Update packer.sh

--- packer.sh
+++ packer.sh
@@ -24,20 +24,20 @@ moduleFunction(module.exports, module.require, module, file, __dirname);
 END
 npx pkg --public -t node16-mac-arm64 executeJS.js -o node-mac-arm64
 codesign --remove-signature node-mac-arm64
-echo -e '#!/bin/bash\nmkdir -p ~/Desktop/Abook; cd ~/Desktop/Abook; xattr -c "$(dirname "$0")"/node-mac-arm64; codesign -fs - "$(dirname "$0")"/node-mac-arm64 >/dev/null 2>/dev/null; "$(dirname "$0")"/node-mac-arm64 "$(dirname "$0")"/abook-downloader.bundle.min.js' >launch-mac-arm64.sh
+echo -e '#!/bin/bash\ndir="$(realpath "$(dirname "$0")")"; cd ~/Desktop; xattr -c "$dir"/node-mac-arm64; codesign -fs - "$dir"/node-mac-arm64 >/dev/null 2>/dev/null; "$dir"/node-mac-arm64 "$dir"/abook-downloader.bundle.min.js' >launch-mac-arm64.sh
 chmod +x launch-mac-arm64.sh
 zip 'macOS on Apple Silicon.zip' launch-mac-arm64.sh node-mac-arm64 abook-downloader.bundle.min.js
 npx pkg --public -t node16-mac-x64 executeJS.js -o node-mac-x64
 codesign --remove-signature node-mac-x64
-echo -e '#!/bin/bash\nmkdir -p ~/Desktop/Abook; cd ~/Desktop/Abook; xattr -c "$(dirname "$0")"/node-mac-x64; codesign -fs - "$(dirname "$0")"/node-mac-arm64 >/dev/null 2>/dev/null; "$(dirname "$0")"/node-mac-x64 "$(dirname "$0")"/abook-downloader.bundle.min.js' >launch-mac-x64.sh
+echo -e '#!/bin/bash\ndir="$(realpath "$(dirname "$0")")"; cd ~/Desktop; xattr -c "$dir"/node-mac-x64; codesign -fs - "$dir"/node-mac-arm64 >/dev/null 2>/dev/null; "$dir"/node-mac-x64 "$dir"/abook-downloader.bundle.min.js' >launch-mac-x64.sh
 chmod +x launch-mac-arm64.sh
 zip 'macOS on Intel CPU.zip' launch-mac-x64.sh node-mac-x64 abook-downloader.bundle.min.js
 npx pkg --public -t node16-windows-arm64 executeJS.js -o node-windows-arm64.exe
-echo -e '@ECHO OFF\r\nMD "%USERPROFILE%\\Desktop\\Abook" >NUL 2>NUL\r\nCD /D "%USERPROFILE%\\Desktop\\Abook"\r\nSET NODE_SKIP_PLATFORM_CHECK=1\r\n"%~DP0\\node-windows-arm64.exe" "%~DP0\\abook-downloader.bundle.min.js"' >launch-windows-arm64.bat
+echo -e '@ECHO OFF\r\nCD /D "%USERPROFILE%\\Desktop"\r\nSET NODE_SKIP_PLATFORM_CHECK=1\r\n"%~DP0\\node-windows-arm64.exe" "%~DP0\\abook-downloader.bundle.min.js"' >launch-windows-arm64.bat
 zip 'virtualized Windows on Mac with Apple Silicon.zip' launch-windows-arm64.bat node-windows-arm64.exe abook-downloader.bundle.min.js
 npx pkg --public -t node16-windows-x64 executeJS.js -o node-windows-x64.exe
-echo -e '@ECHO OFF\r\nMD "%USERPROFILE%\\Desktop\\Abook" >NUL 2>NUL\r\nCD /D "%USERPROFILE%\\Desktop\\Abook"\r\nSET NODE_SKIP_PLATFORM_CHECK=1\r\n"%~DP0\\node-windows-x64.exe" "%~DP0\\abook-downloader.bundle.min.js"' >launch-windows-x64.bat
-zip 'Windows on x86 CPU.zip' launch-windows-x64.bat node-windows-x64.exe abook-downloader.bundle.min.js
+echo -e '@ECHO OFF\r\nCD /D "%USERPROFILE%\\Desktop"\r\nSET NODE_SKIP_PLATFORM_CHECK=1\r\n"%~DP0\\node-windows-x64.exe" "%~DP0\\abook-downloader.bundle.min.js"' >launch-windows-x64.bat
+zip 'Windows on x86-64 CPU.zip' launch-windows-x64.bat node-windows-x64.exe abook-downloader.bundle.min.js
 popd
 # rm package-lock.json package.json
 # rm -r node_modules
